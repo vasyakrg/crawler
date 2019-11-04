5754e5faa6348346339fb488edc496e42729034a
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-11-04 16:55:21 +0700

    dns-init add all records

--- infra/dns-init/main.tf
+++ infra/dns-init/main.tf
@@ -6,8 +6,6 @@ provider "google" {
   # Версия провайдера
   version = "2.11.0"
 
-  # credentials = "${file("~/.config/gcloud/vasya-k8s-1139ce55674f.json")}"
-
   project = "${var.project}"
   region  = "${var.region}"
   zone    = "${var.zone}"
@@ -17,18 +15,42 @@ provider "kubernetes" {
 
 }
 
-
-module "dns" {
-  source        = "./modules/dns"
-  dns_zone_id   = "tfm"
-  dns_zone_name = "tfm.zone"
-  record_name   = "*"
-  record_ip     = "${data.kubernetes_service.nginx.load_balancer_ingress.0.ip}"
-}
-
 data "kubernetes_service" "nginx" {
   metadata {
     name      = "nginx"
     namespace = "nginx-ingress"
   }
 }
+
+module "dns" {
+  source        = "./modules/dns"
+  dns_zone_id   = "tfm"
+  dns_zone_name = "tfm.zone"
+  record_name   = "gitlab"
+  record_ip     = "${data.kubernetes_service.nginx.load_balancer_ingress.0.ip}"
+}
+
+module "dns-registry" {
+  source        = "./modules/dns"
+  dns_zone_id   = "tfm"
+  dns_zone_name = "tfm.zone"
+  record_name   = "registry"
+  record_ip     = "${data.kubernetes_service.nginx.load_balancer_ingress.0.ip}"
+}
+
+module "dns-crawler" {
+  source        = "./modules/dns"
+  dns_zone_id   = "tfm"
+  dns_zone_name = "tfm.zone"
+  record_name   = "crawler"
+  record_ip     = "${data.kubernetes_service.nginx.load_balancer_ingress.0.ip}"
+}
+
+module "dns-prometet" {
+  source        = "./modules/dns"
+  dns_zone_id   = "tfm"
+  dns_zone_name = "tfm.zone"
+  record_name   = "prometheus"
+  record_ip     = "${data.kubernetes_service.nginx.load_balancer_ingress.0.ip}"
+}
+
